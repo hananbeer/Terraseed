64c59cf56fc4a3a55482275784c18be9168d9c86
high_byte <email@hanan.beer>
2023-02-19 13:38:15 +0200

    show labels

--- index.js
+++ index.js
@@ -4,20 +4,23 @@ function devAPI() {
     let el = document.querySelector('img[src="https://maps.gstatic.com/mapfiles/api-3/images/google_gray.svg"]')
     if (!el) {
         setTimeout(devAPI, 10)
+        console.log('...')
         return
     }
     
     el.parentElement.parentElement.remove()
 }
 
+let map = null
 function initMap() {
   const myLatlng = { lat: 41.3029236547479, lng: -81.90172671882485 };
-  const map = new google.maps.Map(document.getElementById("map"), {
+  map = new google.maps.Map(document.getElementById("map"), {
     zoom: 18,
     center: myLatlng,
     heading: 180,
-    mapTypeId: 'satellite'
+    mapTypeId: 'hybrid'
   });
+
   // Create the initial InfoWindow.
   let infoWindow = new google.maps.InfoWindow({
     content: '<div style="text-align: center">Ethereum key for this location:<br />0x493090921ddfa7dbd42497cf19941b968aad2930cce25111921f5777ca2965c7<br /><br />Go hide your keys on Earth!</div>',
@@ -44,5 +47,18 @@ function initMap() {
 
   setTimeout(devAPI, 10)
 }
+
+let timer = null
+function onPointerEvent(e) {
+    clearTimeout(timer)
+    map.setMapTypeId('hybrid')
+    timer = setTimeout(() => {
+        map.setMapTypeId('satellite')
+    }, 2000)
+}
+
+window.addEventListener('pointerdown', onPointerEvent)
+window.addEventListener('wheel', onPointerEvent)
+
 
 window.initMap = initMap;
