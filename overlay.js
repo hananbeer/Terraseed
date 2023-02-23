4684ad8575ca6b18e3a52a3b7463caf10d37d533
high_byte <email@hanan.beer>
2023-02-23 15:00:17 +0200

    updates

--- overlay.js
+++ overlay.js
@@ -1,17 +1,23 @@
 import * as utils from './src/utils.js'
 
-let images = [
-  'beans.jpg',
+let player_images = [
+  // me first :)
+  'hb.png',
+
+  // "streamers / influencers"
   // 'belle.jpg',
+  // 'mia.jpg',
+  // 'sasha.webp',
+
+  // friends
+  'beans.jpg',
   'brock.png',
   'dc.jpg',
   'de.jpg',
   'g.jpg',
   'j.jpg',
   'lib.jpg',
-  // 'mia.jpg',
   'pc.jpg',
-  // 'sasha.webp',
   'tae.png',
   'vec.png',
   'pepe.webp',
@@ -30,7 +36,7 @@ let images = [
   '13.jpg',
   'dump.jpg',
   'riley.jpg',
-  'hb.png'
+  'gm.jpg'
 ]
 
 // cannot change name wtf
@@ -39,54 +45,50 @@ let map
 let is_moon = false
 let players = []
 
-// This example adds hide() and show() methods to a custom overlay's prototype.
-// These methods toggle the visibility of the container <div>.
-// overlay to or from the map.
 function initMap() {
-  let lat = 0.0 // -0.281819
-  let lng = 0.0 // -520.287132
+  let lat = 0.0
+  let lng = 0.0
   let latLng = new google.maps.LatLng(lat, lng)
 
   map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 2.5,
+    zoom: 3,
+    minZoom: 3,
     center: { lat, lng },
     mapTypeId: is_moon ? 'moon' : 'satellite',
-  });
-
-
+  })
 
   if (is_moon) {
     // Normalizes the coords that tiles repeat across the x axis (horizontally)
     // like the standard Google map tiles.
     function getNormalizedCoord(coord, zoom) {
-      const y = coord.y;
-      let x = coord.x;
+      const y = coord.y
+      let x = coord.x
       // tile range in one direction range is dependent on zoom level
       // 0 = 1 tile, 1 = 2 tiles, 2 = 4 tiles, 3 = 8 tiles, etc
-      const tileRange = 1 << zoom;
+      const tileRange = 1 << zoom
 
       // don't repeat across y-axis (vertically)
       if (y < 0 || y >= tileRange) {
-        return null;
+        return null
       }
 
       // repeat across x-axis
       if (x < 0 || x >= tileRange) {
-        x = ((x % tileRange) + tileRange) % tileRange;
+        x = ((x % tileRange) + tileRange) % tileRange
       }
-      return { x: x, y: y };
+      return { x, y }
     }
 
         
     const moonMapType = new google.maps.ImageMapType({
       getTileUrl: function (coord, zoom) {
-        const normalizedCoord = getNormalizedCoord(coord, zoom);
+        const normalizedCoord = getNormalizedCoord(coord, zoom)
 
         if (!normalizedCoord) {
-          return "";
+          return ""
         }
 
-        const bound = Math.pow(2, zoom);
+        const bound = Math.pow(2, zoom)
         return (
           "https://mw1.google.com/mw-planetary/lunar/lunarmaps_v1/clem_bw" +
           "/" +
@@ -96,45 +98,46 @@ function initMap() {
           "/" +
           (bound - normalizedCoord.y - 1) +
           ".jpg"
-        );
+        )
       },
       tileSize: new google.maps.Size(256, 256),
       maxZoom: 9,
-      minZoom: 0,
+      minZoom: 1,
       // @ts-ignore TODO 'radius' does not exist in type 'ImageMapTypeOptions'
       radius: 1738000,
       name: "Moon",
-    });
+    })
 
-    map.mapTypes.set("moon", moonMapType);
-    map.setMapTypeId("moon");
+    map.mapTypes.set("moon", moonMapType)
+    map.setMapTypeId("moon")
   }
 
   // new google.maps.Marker({
   //   position: latLng,
   //   map,
   //   title: "center",
-  // });
+  // })
 
   // new google.maps.Marker({
   //   position: utils.addDistance(latLng, -radius, radius),
   //   map,
   //   title: "minus",
-  // });
+  // })
 
   // new google.maps.Marker({
   //   position: utils.addDistance(latLng, radius, -radius),
   //   map,
   //   title: "plus",
-  // });
+  // })
 
   
-  let base_radius = 1000000
+  const base_radius = 100000
 
   // for (let i = 0; i < 50; i++) {
     // const image = './pics/' + images[(Math.random() * images.length) | 0]
-  for (let i = 0; i < images.length * 3; i++) {
-    let image = './pics/' + images[(i / 3) | 0]
+  // instantiate 3 of each player
+  for (let i = 0; i < player_images.length * 3; i++) {
+    const image = './pics/' + player_images[(i / 3) | 0]
     const dx = (Math.random()*2-1) * 50000000
     const dy = (Math.random()*2-1) * 7000000
     const pos = utils.addDistance(latLng, dx, dy)
@@ -144,22 +147,23 @@ function initMap() {
     players.push(overlay)
   }
   
-  // const toggleButton = document.createElement("button");
-  // toggleButton.textContent = "Toggle";
-  // toggleButton.classList.add("custom-map-control-button");
+  // keep for reference
+  // const toggleButton = document.createElement("button")
+  // toggleButton.textContent = "Toggle"
+  // toggleButton.classList.add("custom-map-control-button")
 
-  // const toggleDOMButton = document.createElement("button");
+  // const toggleDOMButton = document.createElement("button")
 
-  // toggleDOMButton.textContent = "Toggle DOM Attachment";
-  // toggleDOMButton.classList.add("custom-map-control-button");
+  // toggleDOMButton.textContent = "Toggle DOM Attachment"
+  // toggleDOMButton.classList.add("custom-map-control-button")
   // toggleButton.addEventListener("click", () => {
-  //   overlay.toggle();
-  // });
+  //   overlay.toggle()
+  // })
   // toggleDOMButton.addEventListener("click", () => {
-  //   overlay.toggleDOM(map);
-  // });
-  // map.controls[google.maps.ControlPosition.TOP_RIGHT].push(toggleDOMButton);
-  // map.controls[google.maps.ControlPosition.TOP_RIGHT].push(toggleButton);
+  //   overlay.toggleDOM(map)
+  // })
+  // map.controls[google.maps.ControlPosition.TOP_RIGHT].push(toggleDOMButton)
+  // map.controls[google.maps.ControlPosition.TOP_RIGHT].push(toggleButton)
 
   setInterval(minePlayers, 50)
 }
@@ -171,7 +175,7 @@ function minePlayers() {
     if (!player)
       continue
 
-    let seed = utils.minePlayer()
+    let seed = utils.minePlayer(10)
     if (seed) {
       //console.log(seed, utils.keccak256(seed))
       player.setRadius((player.radius * 1.05)) // | 0)
--- src/utils.js
+++ src/utils.js
@@ -19,6 +19,7 @@ export function addDistance(latLng, dx, dy) {
  * the bounds of the image, and a reference to the map.
  */
 export class PlayerOverlay extends google.maps.OverlayView {
+  // js classes are weird
   center;
   image;
   div;
@@ -33,13 +34,12 @@ export class PlayerOverlay extends google.maps.OverlayView {
     this.marker = new google.maps.Marker({
       map,
       title: image.split('.')[0] + ' ' + id,
+      label: image.split('.')[0] + ' ' + id,
     })
 
     this.image = image
     this.setCenter(center)    
     this.setRadius(radius)
-
-    // setInterval(() => this.setRadius(this.radius + 20000), 10)
   }
 
   setCenter(center) {
@@ -53,8 +53,10 @@ export class PlayerOverlay extends google.maps.OverlayView {
   setRadius(radius) {
     // stupid formula that seems to work
     let max_radius = 10000000 / Math.max(1, (Math.abs(this.center.lat())+30) / 30)
+    
     // old formula
     //let max_radius = 5000000 / Math.max(1, Math.abs(this.center.lat()) / 25)
+
     // fixed radius but different visual sizes
     //let max_radius = 2000000
 
@@ -80,48 +82,48 @@ export class PlayerOverlay extends google.maps.OverlayView {
    * added to the map.
    */
   onAdd() {
-    this.div = document.createElement("div");
-    this.div.style.borderStyle = "none";
-    this.div.style.borderWidth = "0px";
-    this.div.style.position = "absolute";
+    this.div = document.createElement("div")
+    this.div.style.borderStyle = "none"
+    this.div.style.borderWidth = "0px"
+    this.div.style.position = "absolute"
 
     // Create the img element and attach it to the div.
-    const img = document.createElement("img");
+    const img = document.createElement("img")
 
-    img.src = this.image;
-    img.style.width = "100%";
-    img.style.height = "100%";
-    img.style.position = "absolute";
+    img.src = this.image
+    img.style.width = "100%"
+    img.style.height = "100%"
+    img.style.position = "absolute"
     img.style.borderRadius = '50%'
-    img.style.opacity = "0.85";
-    img.className = "hover_fade";
-    this.div.appendChild(img);
+    img.style.opacity = "0.85"
+    img.className = "hover_fade"
+    this.div.appendChild(img)
 
     // Add the element to the "overlayLayer" pane.
-    const panes = this.getPanes();
+    const panes = this.getPanes()
 
-    panes.overlayLayer.appendChild(this.div);
+    panes.overlayLayer.appendChild(this.div)
   }
 
+  // Resize the image's div to fit the indicated dimensions.
   draw() {
-    // Resize the image's div to fit the indicated dimensions.
     if (!this.div)
       return
 
     // We use the south-west and north-east
     // coordinates of the overlay to peg it to the correct position and size.
     // To do this, we need to retrieve the projection from the overlay.
-    const overlayProjection = this.getProjection();
+    const overlayProjection = this.getProjection()
     
     // Retrieve the south-west and north-east coordinates of this overlay
     // in LatLngs and convert them to pixel coordinates.
     // We'll use these coordinates to resize the div.
     const sw = overlayProjection.fromLatLngToDivPixel(
       this.bounds.getSouthWest()
-    );
+    )
     const ne = overlayProjection.fromLatLngToDivPixel(
       this.bounds.getNorthEast()
-    );
+    )
 
     let width = ne.x - sw.x
     let height =  sw.y - ne.y
@@ -129,115 +131,63 @@ export class PlayerOverlay extends google.maps.OverlayView {
     // let min = Math.min(Math.abs(width), Math.abs(height))
     // let avg = Math.abs(width) + Math.abs(height)) / 2
 
-    // console.log(width, height)
-    this.div.style.left = (sw.x) + "px";
-    this.div.style.top = (ne.y) + "px";
-    this.div.style.width = width + "px";
-    this.div.style.height = width + "px"; // set height to width to avoid stretching
+    this.div.style.left = sw.x + "px"
+    this.div.style.top = ne.y + "px"
+    this.div.style.width = width + "px"
+    this.div.style.height = width + "px" // set height to width to avoid stretching
   }
+
   /**
    * The onRemove() method will be called automatically from the API if
    * we ever set the overlay's map property to 'null'.
    */
   onRemove() {
     if (this.div) {
-      this.div.parentNode.removeChild(this.div);
-      delete this.div;
+      this.div.parentNode.removeChild(this.div)
+      delete this.div
     }
 
     if (this.marker) {
         this.marker.setMap(null)
-        this.marker.setMap(null)
-        this.marker.setMap(null)
-        this.marker.setMap(null)
-        this.marker.setMap(null)
-        // delete this.marker
+        //delete this.marker
     }
   }
-  /**
-   *  Set the visibility to 'hidden' or 'visible'.
-   */
-  // hide() {
-  //   if (this.div) {
-  //     this.div.style.visibility = "hidden";
-  //   }
-  // }
-  // show() {
-  //   if (this.div) {
-  //     this.div.style.visibility = "visible";
-  //   }
-  // }
-  // toggle() {
-  //   if (this.div) {
-  //     if (this.div.style.visibility === "hidden") {
-  //       this.show();
-  //     } else {
-  //       this.hide();
-  //     }
-  //   }
-  // }
-  // toggleDOM(map) {
-  //   if (this.getMap()) {
-  //     this.setMap(null);
-  //   } else {
-  //     this.setMap(map);
-  //   }
-  // }
 }
 
 
 
 
-
+// 1000 in 10000 chance = 1/10
+// this is more convenient way to represent percentages especially for solidity
 const DIFFICULTY_NUM = BigInt(1000)
 const DIFFICULTY_DEN = BigInt(100000)
 
-function earthDistance2(lat1, lng1, lat2, lng2) {
+// wut
+function earthDistance(lat1, lng1, lat2, lng2) {
   const R = r_earth
-  const φ1 = lat1 * Math.PI / 180; // φ, λ in radians
-  const φ2 = lat2 * Math.PI / 180;
-  const Δφ = (lat2-lat1) * Math.PI / 180;
-  const Δλ = (lng2-lng1) * Math.PI / 180;
+  const φ1 = lat1 * Math.PI / 180 // φ, λ in radians
+  const φ2 = lat2 * Math.PI / 180
+  const Δφ = (lat2-lat1) * Math.PI / 180
+  const Δλ = (lng2-lng1) * Math.PI / 180
 
   const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
             Math.cos(φ1) * Math.cos(φ2) *
-            Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+            Math.sin(Δλ / 2) * Math.sin(Δλ / 2)
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
 
-  const d = R * c; // in metres
+  const d = R * c // in metres
 
   return d
 }
 
-function earthDistance(lat1, lng1, lat2, lng2) {
+// no?
+function earthDistance_2(lat1, lng1, lat2, lng2) {
   return
     Math.acos(
       Math.sin(lat1) * Math.sin(lat2)
       +
       Math.cos(lat1) * Math.cos(lat2) * Math.cos(lng2 - lon1)
     ) * r_earth
-}
-
-export function canDevour(eater, players) {
-  let eaten = []
-  for (let i in players) {
-    let player = players[i]
-    if (!player)
-        continue
-
-    if (eater == player)
-      continue
-    
-    let dist = earthDistance2(player.center.lat(), player.center.lng(), eater.center.lat(), eater.center.lng())
-    // console.log(dist)
-    // debugger
-    if (eater.radius > player.radius && dist < eater.radius * 1.5) {
-      console.log(`eater ${eater.id} can devour player ${player.id}`)
-      eaten.push(player)
-    }
-  }
-
-  return eaten
 }
 
 function hex64(bi) {
@@ -264,4 +214,27 @@ export function minePlayer(count=10) {
     if (seed)
       return seed
   }
+}
+
+
+// larger => shorter distance before larger player devours
+const DEVOUR_DIST_MULTIPLIER = 1.5
+export function canDevour(eater, players) {
+  let eaten = []
+  for (let i in players) {
+    let player = players[i]
+    if (!player)
+        continue
+
+    if (eater == player)
+      continue
+    
+    let dist = earthDistance(player.center.lat(), player.center.lng(), eater.center.lat(), eater.center.lng())
+    if (eater.radius > player.radius && dist < eater.radius * DEVOUR_DIST_MULTIPLIER) {
+    //   console.log(`eater ${eater.id} can devour player ${player.id}`)
+      eaten.push(player)
+    }
+  }
+
+  return eaten
 }
